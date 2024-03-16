cad9d83f0c06af81a4d26be86cf6f4cd621d3f0b
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-03-16 22:49:21 +0530

    commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const randomno=parseInt(Math.random() * 100 +1)
+
+const submit=document.querySelector('#subt')
+
+const userinput=document.querySelector('#guessField')
+
+const guessslot=document.querySelector('.guesses')
+
+const remaining=document.querySelector('.lastResult')
+
+const loworhigh=document.querySelector('.lowOrHi')
+const resultParas=document.querySelector('.resultParas')
+
+const p=document.createElement('p')
+
+let prevGuess= []
+
+let numGuess=1
+
+let playGame=true
+
+function validateGuess(guess)
+{
+    
+}
+
+function checkGuess(guess)
+{
+
+}
+
+function displayGuess(guess)
+{
+
+}
+
+function displayMessage(message)
+{
+
+}
+function newGame()
+{
+
+}
+
+function endGame()
+{
+    
+}
+
